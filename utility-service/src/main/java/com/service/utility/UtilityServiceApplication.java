31b96ba1c759f38c318b7f37f16e1bf509b0792c
= <=>
2021-04-18 22:33:28 +0700

    initial upload file service

---
+++ utility-service/src/main/java/com/service/utility/UtilityServiceApplication.java
@@ -0,0 +1,18 @@
+package com.service.utility;
+
+import com.service.utility.config.FileStorageProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
+public class UtilityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilityServiceApplication.class, args);
+    }
+
+}
