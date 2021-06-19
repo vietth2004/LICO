d9c766edd0384cb6393e0e7bf517ccc4ea856fde
Trần Trung Thành <thanhtt.amg@gmail.com>
2021-06-19 19:58:11 +0700

    add utility service

--- utility-service/src/main/java/com/example/utilityservice/UtilityServiceApplication.java
+++ utility-service/src/main/java/com/example/utilityservice/UtilityServiceApplication.java
@@ -1,13 +1,28 @@
 package com.example.utilityservice;
 
+import com.example.utilityservice.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
+@EnableEurekaClient
 public class UtilityServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UtilityServiceApplication.class, args);
     }
 
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
