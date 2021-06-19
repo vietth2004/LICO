008556f18e6cfa7ac4a50588ca14ad359ddc0f85
Trần Trung Thành <thanhtt.amg@gmail.com>
2021-06-19 17:57:32 +0700

    add utility-service

---
+++ utility-service/src/main/java/com/example/utilityservice/UtilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.utilityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilityServiceApplication.class, args);
+    }
+
+}
