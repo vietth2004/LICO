dd2cec9318c407dd8727ef343b844de113c360f3
Trần Trung Thành <thanhtt.amg@gmail.com>
2021-07-19 10:10:19 +0700

    Refactor

---
+++ cia-service/src/main/java/com/example/ciaservice/CiaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ciaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiaServiceApplication.class, args);
+    }
+
+}
