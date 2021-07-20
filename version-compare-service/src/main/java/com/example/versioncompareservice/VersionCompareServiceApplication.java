21f461d281313e3b8153afd4986aa011f9d296d1
Trần Trung Thành <thanhtt.amg@gmail.com>
2021-07-20 16:20:53 +0700

    Add version comparison service

---
+++ version-compare-service/src/main/java/com/example/versioncompareservice/VersionCompareServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.versioncompareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionCompareServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VersionCompareServiceApplication.class, args);
+    }
+
+}
