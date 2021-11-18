e14c59731e2825c474d1c7f8e81fe537a679b59e
thdtt <thanhdat.vnu@gmail.com>
2021-11-18 20:06:11 +0700

    refactor code

--- xml-service/src/main/java/com/jcia/xmlservice/Controller/XmlController.java
+++ xml-service/src/main/java/com/jcia/xmlservice/Controller/XmlController.java
@@ -1,56 +1,27 @@
 package com.jcia.xmlservice.Controller;
 
 import com.jcia.xmlservice.Dom.Node;
-import com.jcia.xmlservice.Parser.XmlFileParser;
-import com.jcia.xmlservice.Utils.Exception.JciaNotFoundException;
-import com.jcia.xmlservice.Utils.Helper.FileHelper;
-import com.jcia.xmlservice.Utils.Helper.StringHelper;
-import com.jcia.xmlservice.Utils.Log.ClientLevel;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.jcia.xmlservice.Service.XmlService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class XmlController {
 
-    private Logger logger = LogManager.getLogger(XmlController.class);
+    @Autowired
+    private XmlService xmlService;
 
-    @PostMapping("/api/xml-parser")
-    public ResponseEntity<?> getXmlTree(@RequestBody String folderPath) throws IOException {
-        XmlFileParser xmlFileParser = new XmlFileParser();
-        List<Node> nodes = new ArrayList<>();
-
-//        folderPath = "C:/Users/thanh/Documents/jcia-ezc";
-
-        Path path = Paths.get(folderPath);
-        List<Path> paths = FileHelper.listFiles(path);
-        paths.forEach(x -> {
-            if(StringHelper.SUPPORTED_EXTENSIONS.contains(FileHelper.getFileExtension(x.toString()))){
-                try {
-                    Node parsedNode = xmlFileParser.parse(x.toString());
-                    if(parsedNode != null){
-//                    Map<String, Node> elem = new HashMap<>();
-//                    elem.put(getFileExtension(parsedNode.getAbsolutePath()), parsedNode);
-//                    nodes.add(elem);
-                        nodes.add(parsedNode);
-                    }
-                } catch (JciaNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        logger.log(ClientLevel.CLIENT, nodes.stream().toArray().length);
-
+    @PostMapping("/api/pathParse")
+    public ResponseEntity<?> parseProjectByPath(@RequestBody String folderPath) throws IOException {
+        List<Node> nodes = xmlService.parseProjectWithPath(folderPath);
         return new ResponseEntity<Object>(nodes,HttpStatus.OK);
     }
 
