015ff20a134ccdf6d15b2b602a680d83251512f7
thdtt <thanhdat.vnu@gmail.com>
2021-12-26 17:25:28 +0700

    update docker compose and build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+mvn -T 1C clean install compile package -f .\discovery-server\pom.xml -DskipTests
+mvn -T 1C clean install compile package -f .\cia-service\pom.xml -DskipTests
+mvn -T 1C clean install compile package -f .\file-service\pom.xml -DskipTests
+mvn -T 1C clean install compile package -f .\github-service\pom.xml -DskipTests
+mvn -T 1C clean install compile package -f .\java-service\pom.xml -DskipTests
+mvn -T 1C clean install compile package -f .\parser-service\pom.xml -DskipTests
+mvn -T 1C clean install compile package -f .\project-service\pom.xml -DskipTests
+mvn -T 1C clean install compile package -f .\spring-service\pom.xml -DskipTests
+mvn -T 1C clean install compile package -f .\user-service\pom.xml -DskipTests
+mvn -T 1C clean install compile package -f .\utility-service\pom.xml -DskipTests
+mvn -T 1C clean install compile package -f .\version-compare-service\pom.xml -DskipTests
+mvn -T 1C clean install compile package -f .\xml-service\pom.xml -DskipTests
