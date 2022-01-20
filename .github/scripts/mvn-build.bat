484022f18189977e58ea07a1af841e1d49fe8db6
thdtt <thanhdat.vnu@gmail.com>
2022-01-20 08:08:32 +0700

    add check environment for windows based system (#52)
    * Create mvn-build.bat
    
    * Create mvn-verify.bat
    
    * Update jcia-ci.yml
    
    * Update jcia-ci.yml
    
    change action for all branches

---
+++ .github/scripts/mvn-build.bat
@@ -0,0 +1,13 @@
+call mvn -T 1C clean install compile package -f .\discovery-server\pom.xml
+call mvn -T 1C clean install compile package -f .\cia-service\pom.xml
+call mvn -T 1C clean install compile package -f .\file-service\pom.xml
+call mvn -T 1C clean install compile package -f .\github-service\pom.xml 
+call mvn -T 1C clean install compile package -f .\java-service\pom.xml
+call mvn -T 1C clean install compile package -f .\parser-service\pom.xml 
+call mvn -T 1C clean install compile package -f .\project-service\pom.xml
+call mvn -T 1C clean install compile package -f .\spring-service\pom.xml
+call mvn -T 1C clean install compile package -f .\user-service\pom.xml
+call mvn -T 1C clean install compile package -f .\utility-service\pom.xml
+call mvn -T 1C clean install compile package -f .\version-compare-service\pom.xml
+call mvn -T 1C clean install compile package -f .\xml-service\pom.xml
+call mvn -T 1C clean install compile package -f .\api-gateway\pom.xml
