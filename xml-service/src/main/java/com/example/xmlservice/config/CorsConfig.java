ff774d55ff461e82c646fba9c939a1d8ca3bd7be
thdtt <thanhdat.vnu@gmail.com>
2022-02-13 18:20:35 +0700

    disable cors per service, only allow cors in gateway

--- xml-service/src/main/java/com/example/xmlservice/config/CorsConfig.java
+++ xml-service/src/main/java/com/example/xmlservice/config/CorsConfig.java
@@ -6,22 +6,22 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
-public class CorsConfig {
-
-    /**
-     * Open cors for frontend
-     * @return
-     */
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
-                        .allowedHeaders("*");
-            }
-        };
-    }
-
-}
+//@Configuration
+//public class CorsConfig {
+//
+//    /**
+//     * Open cors for frontend
+//     * @return
+//     */
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+//                        .allowedHeaders("*");
+//            }
+//        };
+//    }
+//
+//}
