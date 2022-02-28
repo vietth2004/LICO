23b677fb76fd5112b922c7408911a060b95e8205
Trần Trung Thành <thanhtt.amg@gmail.com>
2022-02-28 19:39:28 +0700

    Change to Java unzip

--- version-compare-service/src/main/java/com/example/versioncompareservice/service/FileStorageService.java
+++ version-compare-service/src/main/java/com/example/versioncompareservice/service/FileStorageService.java
@@ -14,6 +14,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
@@ -21,6 +23,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 @Service
 public class FileStorageService {
@@ -60,7 +64,7 @@ public class FileStorageService {
             // Copy file to the target location (Replacing existing file with the same name)
             Path targetLocation = this.fileStorageLocation.resolve(zipFilePath);
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
-            unzipFile(filePath, folderPath);
+            javaUnzipFile(filePath, folderPath);
 
             return fileName;
         } catch (IOException ex) {
@@ -107,5 +111,59 @@ public class FileStorageService {
             e.printStackTrace();
         }
         return DestinationFolderPath;
+    }
+
+    public String javaUnzipFile(String Filepath, String DestinationFolderPath) {
+        try {
+            String fileZip = Filepath;
+            File destDir = new File(DestinationFolderPath);
+            byte[] buffer = new byte[1024];
+            ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+            ZipEntry zipEntry = zis.getNextEntry();
+            while (zipEntry != null) {
+                // ...
+                while (zipEntry != null) {
+                    File newFile = newFile(destDir, zipEntry);
+                    if (zipEntry.isDirectory()) {
+                        if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                            throw new IOException("Failed to create directory " + newFile);
+                        }
+                    } else {
+                        // fix for Windows-created archives
+                        File parent = newFile.getParentFile();
+                        if (!parent.isDirectory() && !parent.mkdirs()) {
+                            throw new IOException("Failed to create directory " + parent);
+                        }
+
+                        // write file content
+                        FileOutputStream fos = new FileOutputStream(newFile);
+                        int len;
+                        while ((len = zis.read(buffer)) > 0) {
+                            fos.write(buffer, 0, len);
+                        }
+                        fos.close();
+                    }
+                    zipEntry = zis.getNextEntry();
+                }
+            }
+            zis.closeEntry();
+            zis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return DestinationFolderPath;
+    }
+
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
     }
 }
