89edd345255e0d1bf69e817a65bab15caa8aa3f7
Trần Trung Thành <thanhtt.amg@gmail.com>
2022-03-29 14:50:36 +0700

    Added Spring xml dependency

--- processing-service/cia-service/src/main/java/com/example/ciaservice/Utility/Getter.java
+++ processing-service/cia-service/src/main/java/com/example/ciaservice/Utility/Getter.java
@@ -12,14 +12,26 @@ public class Getter {
     public static Set<Node> gatherImpactFromDependencies(List<Node> nodes, List<JavaNode> javaNodes, Integer totalNodes, JavaNode changedNode, Set<Node> affectedNodes) {
 
         for(Pair dependency : changedNode.getDependencyFrom()) {
-            JavaNode javaNode = Searcher.findJavaNode(javaNodes, dependency.getNode().getId());
-            Node node = Searcher.findNode(nodes, dependency.getNode().getId());
-            if(!affectedNodes.contains(node)){
-                affectedNodes.add(Searcher.findNode(nodes, dependency.getNode().getId()));
-                affectedNodes.addAll(gatherImpactFromDependencies(nodes, javaNodes, totalNodes, javaNode, affectedNodes));
+            if(dependency.getDependency().getMEMBER() < 1) {
+                gatherImpact(nodes, javaNodes, totalNodes, affectedNodes, dependency);
+            }
+        }
+
+        for(Pair dependency : changedNode.getDependencyTo()) {
+            if(dependency.getDependency().getOVERRIDE() > 0) {
+                gatherImpact(nodes, javaNodes, totalNodes, affectedNodes, dependency);
             }
         }
 
         return affectedNodes;
+    }
+
+    private static void gatherImpact(List<Node> nodes, List<JavaNode> javaNodes, Integer totalNodes, Set<Node> affectedNodes, Pair dependency) {
+        JavaNode javaNode = Searcher.findJavaNode(javaNodes, dependency.getNode().getId());
+        Node node = Searcher.findNode(nodes, dependency.getNode().getId());
+        if(!affectedNodes.contains(node)){
+            affectedNodes.add(Searcher.findNode(nodes, dependency.getNode().getId()));
+            affectedNodes.addAll(gatherImpactFromDependencies(nodes, javaNodes, totalNodes, javaNode, affectedNodes));
+        }
     }
 }
