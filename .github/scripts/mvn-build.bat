88227fd0ec89a8c23b89f43e12287c6ca26d6627
lo9876576435 <24020203@vnu.edu.vn>
2025-12-25 22:31:44 +0700

    fix build script

--- .github/scripts/mvn-build.bat
+++ .github/scripts/mvn-build.bat
@@ -1,19 +1,40 @@
-call mvn -T 1C clean install compile package -f .\core-engine\core\pom.xml
-call mvn -T 1C clean install compile package -f .\core-engine\jdt\pom.xml
-call mvn -T 1C clean install compile package -f .\core-engine\utils\pom.xml
-call mvn -T 1C clean install compile package -f .\management-service\discovery-server\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\processing-service\cia-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\back-end-service\file-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\back-end-service\git-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\processing-service\java-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\back-end-service\parser-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\back-end-service\project-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\processing-service\spring-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\back-end-service\user-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\processing-service\utility-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\processing-service\version-compare-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\processing-service\jsf-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\management-service\api-gateway\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\processing-service\struts-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\processing-service\jsp-service\pom.xml -DskipTests
-call mvn -T 1C clean install compile package -f .\processing-service\xml-service\pom.xml -DskipTests
+@echo off
+setlocal
+
+call mvn -B -ntp -T 1C -f .\core-engine\core\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\core-engine\jdt\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\core-engine\utils\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+
+call mvn -B -ntp -T 1C -f .\processing-service\cia-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\back-end-service\file-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\back-end-service\git-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\processing-service\java-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\back-end-service\parser-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\back-end-service\project-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\processing-service\spring-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\back-end-service\user-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\processing-service\utility-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\processing-service\version-compare-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\processing-service\jsf-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\processing-service\struts-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\processing-service\jsp-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+call mvn -B -ntp -T 1C -f .\processing-service\xml-service\pom.xml clean install -DskipTests
+if errorlevel 1 exit /b 1
+
+endlocal
--- .github/scripts/mvn-build.sh
+++ .github/scripts/mvn-build.sh
@@ -1,20 +1,21 @@
-#!bin/bash
-mvn -T 1C clean install compile package -f ./core-engine/core/pom.xml
-mvn -T 1C clean install compile package -f ./core-engine/jdt/pom.xml
-mvn -T 1C clean install compile package -f ./core-engine/utils/pom.xml
-mvn -T 1C clean install compile package -f ./management-service/discovery-server/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./processing-service/cia-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./back-end-service/file-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./back-end-service/git-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./processing-service/java-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./back-end-service/parser-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./back-end-service/project-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./processing-service/spring-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./back-end-service/user-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./processing-service/utility-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./processing-service/version-compare-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./processing-service/jsf-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./management-service/api-gateway/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./processing-service/struts-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./processing-service/jsp-service/pom.xml -DskipTests
-mvn -T 1C clean install compile package -f ./processing-service/xml-service/pom.xml -DskipTests
+#!/usr/bin/env bash
+set -euo pipefail
+
+mvn -B -ntp -T 1C -f ./core-engine/core/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./core-engine/jdt/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./core-engine/utils/pom.xml clean install -DskipTests
+
+mvn -B -ntp -T 1C -f ./processing-service/cia-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./back-end-service/file-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./back-end-service/git-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./processing-service/java-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./back-end-service/parser-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./back-end-service/project-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./processing-service/spring-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./back-end-service/user-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./processing-service/utility-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./processing-service/version-compare-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./processing-service/jsf-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./processing-service/struts-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./processing-service/jsp-service/pom.xml clean install -DskipTests
+mvn -B -ntp -T 1C -f ./processing-service/xml-service/pom.xml clean install -DskipTests
